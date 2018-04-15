df1c76174f16b3a46977a584bd8722d70adce722
Matthew Parker <matt.parker@au.tribalworldwide.com>
2018-04-16 09:32:24 +1000

    first commit

---
+++ nuget-test/nuget-test/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nuget_test
+{
+    public class MainClass : IMainClass
+    {
+
+        //create package
+        //https://docs.microsoft.com/en-us/nuget/guides/create-net-standard-packages-vs2015
+        //publish the package
+        //https://docs.microsoft.com/en-us/nuget/create-packages/publish-a-package
+
+
+        public string GetHelloWorld()
+        {
+            return "hello world";
+        }
+    }
+}
